cfea9731dd50a36e4f9ad0657058598b3aa21bdc
gennpan <panicogennaro99@gmail.com>
2022-03-11 20:26:41 +0100

    (generic-add) commit

--- Naulum/src/application/Main.java
+++ Naulum/src/application/Main.java
@@ -1,18 +1,26 @@
 package application;
+
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Parent root = FXMLLoader.load(getClass().getResource("/fxml/loginMain.fxml"));
+			Scene scene = new Scene(root);
+			
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			//this because with the other method it's recommended.
+			
+			String css = this.getClass().getResource("/css/mainStyle.css").toExternalForm();
+			scene.getStylesheets().add(css);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
