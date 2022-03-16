2e2552a7ddf94bcb70a2fdbedd8eb528f050b95a
gennpan <panicogennaro99@gmail.com>
2022-03-16 15:36:57 +0100

    logo - commit

--- Naulum/src/com/naulum/fxgui/Main.java
+++ Naulum/src/com/naulum/fxgui/Main.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 
 
 
@@ -23,6 +24,7 @@ public class Main extends Application {
 			
 			String css = this.getClass().getResource("/com/naulum/fxgui/css/style.css").toExternalForm();
 			scene.getStylesheets().add(css);
+			stage.getIcons().add(new Image("/com/naulum/fxgui/png/logo.png"));
 			stage.setScene(scene);
 			stage.show();
 
